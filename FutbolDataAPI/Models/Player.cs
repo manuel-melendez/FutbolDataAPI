a26d076fe7ec5a644466d87483373f2286b5cf0f
Manuel Melendez <mannymelendez1213@gmail.com>
2025-02-10 11:54:14 -0500

    Modified Player and Club relationship

--- FutbolDataAPI/Models/Player.cs
+++ FutbolDataAPI/Models/Player.cs
@@ -1,11 +1,18 @@
-﻿namespace FutbolDataAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FutbolDataAPI.Models
 {
     public class Player
     {
-        public int Id { get; set; }
+        [Key]
+        public int PlayerId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
         public int Number { get; set; }
+        [ForeignKey("Club")]
+        public int ClubId { get; set; }
+        public Club Club { get; set; }
     }
 }
