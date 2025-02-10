db466c45b64e6501c938cfeed9003ae2d237d728
Manuel Melendez <mannymelendez1213@gmail.com>
2025-02-10 11:45:04 -0500

    Added Db context and Player.cs class

---
+++ FutbolDataAPI/Models/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace FutbolDataAPI.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public int Number { get; set; }
+    }
+}
