cae8bb01cb175d873685fcd6548a2b1cc5056cac
Manuel Melendez <mannymelendez1213@gmail.com>
2025-02-10 14:07:54 -0500

    Created Club Repo and Interface

---
+++ FutbolDataAPI/Repositories/ClubRepository.cs
@@ -0,0 +1,61 @@
+﻿using FutbolDataAPI.Data;
+using FutbolDataAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FutbolDataAPI.Repositories
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Club> CreateClub(Club club)
+        {
+            _context.Clubs.Add(club);
+            await _context.SaveChangesAsync();
+
+            return club;
+        }
+
+        public async Task DeleteClub(int clubId)
+        {
+            var club = await _context.Clubs.FirstOrDefaultAsync(c => c.ClubId == clubId);
+            if (club != null)
+            {
+                _context.Clubs.Remove(club);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Club> GetClubById(int clubId)
+        {
+            var club = await _context.Clubs.FirstOrDefaultAsync(c => c.ClubId == clubId);
+            return club;
+        }
+
+        public async Task<IEnumerable<Club>> GetClubs()
+        {
+            var clubs = await _context.Clubs.ToListAsync();
+            return clubs;
+        }
+
+        public async Task<Club> UpdateClub(Club club)
+        {
+            var existingClub = await _context.Clubs.FirstOrDefaultAsync(c => c.ClubId == club.ClubId);
+            if (existingClub != null)
+            {
+                existingClub.StadiumName = club.StadiumName;
+                existingClub.City = club.City;
+                existingClub.Country = club.Country;
+
+                await _context.SaveChangesAsync();
+                return existingClub;
+            }
+            return null;
+        }
+    }
+}
